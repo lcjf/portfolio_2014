38b90a2497ef2719c36b489abcc02f3e96d270bd
lcjf <jongo1@gmail.com>
2015-03-25 23:05:36 +0000

    added youtube API with functionality to create public playlist

---
+++ app_scripts/functions.js
@@ -0,0 +1,11 @@
+/*function getTitle(data) {
+    var feed = data.feed;
+    var entries = feed.entry || [];
+    for (var i = 0; i < entries.length; i++) {
+        var entry = entries[i];
+        var title = entry.title.$t;
+    }
+    console.log(entries);
+    console.log(entry);
+    alert("test");
+} */
